5e30494886599fe47f817965493c11bdbd5a6a43
Jeroen Desloovere <jeroendesloovere@gmail.com>
2015-05-26 17:45:29 +0200

    example updated

--- examples/credentials.php
+++ examples/credentials.php
@@ -1,11 +1,15 @@
 <?php
 
 // define credentials for the fork cms based website you try to connect to
-$url = ''; // required
+$url = ''; // required, example: http://www.mydomainname.tld/api/v1/
 $email = ''; // required
-$apiKey = ''; // required
 
-// username and password are required
-if (empty($url) || empty($email) || empty($apiKey)) {
-    echo 'Please define your url, email or app key in ' . __DIR__ . '/credentials.php';
+// calls with authentication (we use the $password to trade it in for an $apiKey)
+$password = ''; // we only need to set a password if we try to call functions which require authentication
+// or
+$apiKey = ''; // if you know your personal apiKey, fill it in, otherwise you need to fill in the password
+
+// url and email are required
+if (empty($url) || empty($email)) {
+    echo 'Please define your url and email in ' . __DIR__ . '/credentials.php';
 }
--- examples/example.php
+++ examples/example.php
@@ -6,12 +6,42 @@ require_once __DIR__ . '/credentials.php';
 
 use \ForkCms\Api\ForkCms;
 
-// create instance
-$api = new ForkCms($url, $email, $apiKey);
+
+// FIRST CONNECTION: getting API key so we can call functions which require authentication
+// if we use a database we can save the $apiKey and skip this step the next time.
+
+// init fork cms
+$api = new ForkCms(
+    $url,
+    $email
+);
 
 try {
-//    $response = $api->coreGetAPIKey(EMAIL, PASSWORD);
+    // we get the api key for this user 
+    $response = $api->coreGetAPIKey($email, $password);
 //    $response = $api->coreGetInfo();
+
+    // we need to save the api key in our database
+    $apiKey = $response['api_key'];
+
+    // we can set the api key or create a new Fork CMS Api instead.
+    $api->setApiKey($apiKey);
+} catch (Exception $e) {
+    var_dump($e);
+}
+
+// output
+var_dump($response);
+
+// we can create a new Fork CMS API when we have the $apiKey
+$api = new ForkCms(
+    $url,
+    $email,
+    $apiKey
+);
+
+try {
+// authorisation required for the following functions
 //    $response = $api->coreAppleAddDevice(APPLE_DEVICE_TOKEN);
 //    $response = $api->coreAppleRemoveDevice(APPLE_DEVICE_TOKEN);
 //    $response = $api->blogCommentsGet();
@@ -24,3 +54,4 @@ try {
 
 // output
 var_dump($response);
+
